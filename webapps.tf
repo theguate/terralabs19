58833affdb88a851bce4bbab8810a7d1f0e48e1c
Javier Galvez <javier@BIRCHSURFACE.redmond.corp.microsoft.com>
2019-07-04 12:21:42 +0800

    lab4

--- webapps.tf
+++ webapps.tf
@@ -12,7 +12,7 @@ resource "random_string" "webapprnd" {
   special = false
 }
 
-resource "azurerm_app_service_plan" "free" {
+resource "azurerm_app_service_plan" "standard" {
     count               = "${length(var.webapplocs)}"
     name                = "plan-free-${var.webapplocs[count.index]}"
     location            = "${var.webapplocs[count.index]}"
@@ -21,8 +21,8 @@ resource "azurerm_app_service_plan" "free" {
 
     kind                = "Linux"
     sku {
-        tier = "Free"
-        size = "F1"
+        tier = "Standard"
+        size = "S1"
     }
     properties {
     reserved = true
@@ -36,7 +36,7 @@ resource "azurerm_app_service" "citadel" {
     resource_group_name = "${azurerm_resource_group.webapps.name}"
     tags                = "${azurerm_resource_group.webapps.tags}"
 
-    app_service_plan_id = "${element(azurerm_app_service_plan.free.*.id, count.index)}"
+    app_service_plan_id = "${element(azurerm_app_service_plan.standard.*.id, count.index)}"
 }
 
 output "webapp_ids" {
