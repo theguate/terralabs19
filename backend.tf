96babee173e6861742835b036fa0c7a63bebb0d6
Javier Galvez <galvezjavier@hotmail.com>
2019-07-08 11:49:49 +0800

    lab5

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "azurerm" {
+      storage_account_name = "tfstateqc"
+      container_name       = "tfstate-691d05c0-459b-4483-8acf-dd57427f1852-terralabs19"
+      key                  = "terraform.tfstate"
+      access_key           = "tVidc08QkqzY9ypFmA4yil9qK8E8fwbvqq09sKLlCwjXQhBGIq45izwgD3dIwuWGLza8bh5lAUJsN6GIz7TzSg=="
+    }
+}
